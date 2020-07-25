7e6dc763a9f49f79086bcd62d58295c59e3360ee
Michael Le <hle.michael@gmail.com>
2020-07-25 22:26:49 +0100

    added xcframework

---
+++ TestlioSDK.xcframework/ios-x86_64-simulator/TestlioSDK.framework/Headers/TestlioSDK.h
@@ -0,0 +1,19 @@
+//
+//  TestlioSDK.h
+//  TestlioSDK
+//
+//  Created by Michael Le on 01/07/2020.
+//  Copyright © 2020 Appy Coder Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestlioSDK.
+FOUNDATION_EXPORT double TestlioSDKVersionNumber;
+
+//! Project version string for TestlioSDK.
+FOUNDATION_EXPORT const unsigned char TestlioSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestlioSDK/PublicHeader.h>
+
+
