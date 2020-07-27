5ffcf0f8f7c850809c656d870fce87d631486276
Michael Le <michael@appycoder.com>
2020-07-27 12:59:00 +0100

    add package swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "TestlioSDK",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "TestlioSDK", 
+            targets: ["TestlioSDK"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "TestlioSDK", 
+            path: "TestlioSDK.xcframework")
+    ])
