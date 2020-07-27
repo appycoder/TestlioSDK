304ead9719e2e24394d48e5ad691dad0d795ae62
Michael Le <michael@appycoder.com>
2020-07-27 13:24:42 +0100

    build using xcode 12 beta

--- Package.swift
+++ Package.swift
@@ -2,17 +2,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "TestlioSDK",
-    platforms: [
-        .iOS(.v11)
-    ],
-    products: [
-        .library(
-            name: "TestlioSDK", 
-            targets: ["TestlioSDK"])
-    ],
-    targets: [
-        .binaryTarget(
-            name: "TestlioSDK", 
-            path: "TestlioSDK.xcframework")
-    ])
+  name: "TestlioSDK",
+  platforms: [
+    .iOS(.v11)
+  ],
+  products: [
+    .library(
+      name: "TestlioSDK",
+      targets: ["TestlioSDK"])
+  ],
+  dependencies: [
+    .package(
+      url: "https://github.com/microsoft/plcrashreporter.git",
+      from: Version(1, 7, 1)
+    )
+  ],
+  targets: [
+    .binaryTarget(
+      name: "TestlioSDK",
+      path: "TestlioSDK.xcframework")
+])
